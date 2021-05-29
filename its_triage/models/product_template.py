fdb276864f9c1e91ef1bc77f367354e16553bd64
solo-jr <jeanrobertr0@gmai.com>
2021-05-30 00:40:34 +0300

    [ADD] Default values managers constrains

--- its_triage/models/product_template.py
+++ its_triage/models/product_template.py
@@ -19,7 +19,8 @@
 #
 ##############################################################################
 
-from odoo import fields, models, api
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
@@ -49,3 +50,26 @@ class ProductTemplate(models.Model):
     ss_product_output_ids = fields.Many2many("product.product", relation="ss_product_output_ids_rel",
                                              string="Product output",
                                              domain="[('product_tmpl_id', '=', id)]")
+
+    @api.constrains('is_dv_automatic_sorting', 'is_dv_manual_sorting', 'is_dv_smalls_sorting')
+    def _verify_number_default_values(self):
+        is_dv_automatic_sorting = self.env['product.template'].search([('is_dv_automatic_sorting', '=', True)])
+        is_dv_manual_sorting = self.env['product.template'].search([('is_dv_manual_sorting', '=', True)])
+        is_dv_smalls_sorting = self.env['product.template'].search([('is_dv_smalls_sorting', '=', True)])
+        for rec in self:
+            if rec.is_dv_automatic_sorting is True and len(is_dv_automatic_sorting) > 1:
+                others = is_dv_automatic_sorting.filtered(lambda r: r.id != rec.id)
+                raise ValidationError(
+                    _("{} is already used as the default value for automatic sorting").format(others[0].display_name)
+                )
+            if rec.is_dv_manual_sorting is True and len(is_dv_manual_sorting) > 1:
+                others = is_dv_manual_sorting.filtered(lambda r: r.id != rec.id)
+                raise ValidationError(
+                    _("{} is already used as the default value for manual sorting").format(others[0].display_name)
+                )
+            if rec.is_dv_smalls_sorting is True and len(is_dv_smalls_sorting) > 1:
+                others = is_dv_smalls_sorting.filtered(lambda r: r.id != rec.id)
+                raise ValidationError(
+                    _("{} is already used as the default value for smalls sorting").format(others[0].display_name)
+                )
+
